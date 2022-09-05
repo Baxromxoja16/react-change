f055c5a265403b85a6545b6386a9339b26450273
Baxromxoja16 <aglamkhujaev@email.com>
2022-09-05 13:36:27 +0500

    feat: courses media query comleted

--- src/containers/CoursesPage/Course/Course.jsx
+++ src/containers/CoursesPage/Course/Course.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Course.sass'
-import courseImg from "../../../images/course.png"
+import img1 from "../../../images/course.png"
+import img2 from "../../../images/course.png"
 
 const Course = () => {
 
@@ -8,29 +9,31 @@ const Course = () => {
         {
             title: "Java Development",
             text: "Курсы программирования Java — современная комплексная программа обучения языку Java. Java – популярный язык программирования, на котором кодят игровые и мобильные приложения, десктоп-программы и огромное количество soft-продуктов для предприятий.",
+            img: img1
         },
         {
             title: "PHP Development",
             text: "Курсы PHP — это комплексная двухуровневая программа обучения PHP,  после изучения которой вы сможете претендовать на вакансию Junior-специалиста и зарплату от 300 $/мес.",
+            img: img2
         },
         {
             title: "Java Development",
             text: "Курсы программирования Java — современная комплексная программа обучения языку Java. Java – популярный язык программирования, на котором кодят игровые и мобильные приложения, десктоп-программы и огромное количество soft-продуктов для предприятий.",
+            img: img1
         },
         {
             title: "PHP Development",
             text: "Курсы PHP — это комплексная двухуровневая программа обучения PHP,  после изучения которой вы сможете претендовать на вакансию Junior-специалиста и зарплату от 300 $/мес.",
+            img: img2
         }
     ]
 
     let answw = info.map((x, i) => {
-        let title = x.title
-        let text = x.text
         let answ =
-            <div div className={i % 2 !== 0 ? "row flex-direction-reverse" : "row" } key={i}>
+            <div div className={i % 2 !== 0 ? "row flex-direction-reverse" : "row"} key={i}>
                 <div className="course_left_info">
-                    <h2>{title}</h2>
-                    <p className='course_text'>{text}</p>
+                    <h2>{x.title}</h2>
+                    <p className='course_text'>{x.text}</p>
                     <div className="course_buttons">
                         <a href='#!' className='about_course'>о курсе</a>
                         <div className='line'></div>
@@ -38,7 +41,7 @@ const Course = () => {
                     </div>
                 </div>
                 <div className="course_right_img">
-                    <img src={courseImg} alt="png" />
+                    <img src={x.img} alt={x.img} />
                 </div>
             </div >
         return answ
