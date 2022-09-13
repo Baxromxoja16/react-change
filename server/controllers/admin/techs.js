08bea7e536029e93023de00680e05164373b822b
xayitboyev-dev <xayitboyev.dev@gmail.com>
2022-09-13 14:46:19 +0500

    feat: lessons crud

--- server/controllers/admin/techs.js
+++ server/controllers/admin/techs.js
@@ -1,36 +1,38 @@
-const Techs = require('../../models/Techs');
+const Tech = require("../../models/Techs");
 
 module.exports.getAll = async (req, res) => {
-    const techs = await Techs.find();
-    res.json({ ok: true, techs });
-}
+  const techs = await Tech.find();
+  res.json({ ok: true, techs });
+};
 
 module.exports.addNew = async (req, res) => {
-    try {
-        if (req.file) req.body.image = '/uploads/techs/' + req.file.filename;
-        await Techs.create(req.body);
-        res.json({ ok: true, message: 'Item is successfully created!' });
-    } catch (message) {
-        res.status(400).json({ ok: false, message });
-    }
-}
+  try {
+    if (req.file) req.body.image = "/uploads/techs/" + req.file.filename;
+    await Tech.create(req.body);
+    res.json({ ok: true, message: "Item is successfully created!" });
+  } catch (message) {
+    res.status(400).json({ ok: false, message });
+  };
+};
 
 module.exports.updateOne = async (req, res) => {
-    try {
-        const updated = await Techs.findByIdAndUpdate(req.params.id, req.body);
-        if (updated) res.json({ ok: true, message: 'Item is successfully updated!' });
-        else throw '';
-    } catch (message) {
-        res.status(400).json({ ok: false, message: 'Item is not found!' });
-    }
+  try {
+    const updated = await Tech.findByIdAndUpdate(req.params.id, req.body);
+    if (updated)
+      res.json({ ok: true, message: "Item is successfully updated!" });
+    else throw "";
+  } catch (message) {
+    res.status(400).json({ ok: false, message: "Item is not found!" });
+  };
 };
 
 module.exports.deleteOne = async (req, res) => {
-    try {
-        const deleted = await Techs.findByIdAndDelete(req.params.id);
-        if (deleted) res.json({ ok: true, message: 'Item is successfully deleted!' });
-        else throw '';
-    } catch (message) {
-        res.status(400).json({ ok: false, message: 'Item is not found!' });
-    }
+  try {
+    const deleted = await Tech.findByIdAndDelete(req.params.id);
+    if (deleted)
+      res.json({ ok: true, message: "Item is successfully deleted!" });
+    else throw "";
+  } catch (message) {
+    res.status(400).json({ ok: false, message: "Item is not found!" });
+  };
 };
